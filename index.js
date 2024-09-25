1612e2257a0469d6950622396910bd134fed3186
Bonsuano <david@compto.com>
2024-09-24 22:59:44 -0500

    abomination

--- index.js
+++ index.js
@@ -1,19 +1,105 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+// "use strict";
+// var __createBinding = (this && this.__createBinding) || (Object.create ? (function (o, m, k, k2) {
+//     if (k2 === undefined) k2 = k;
+//     var desc = Object.getOwnPropertyDescriptor(m, k);
+//     if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+//         desc = { enumerable: true, get: function () { return m[k]; } };
+//     }
+//     Object.defineProperty(o, k2, desc);
+// }) : (function (o, m, k, k2) {
+//     if (k2 === undefined) k2 = k;
+//     o[k2] = m[k];
+// }));
+// var __exportStar = (this && this.__exportStar) || function (m, exports) {
+//     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+// };
+// exports.__esModule = true;
+// __exportStar(require("./lib/accounts.js"), exports);
+// __exportStar(require("./lib/constants.js"), exports);
+// __exportStar(require("./lib/instruction.js"), exports);
+
+const {
+    COMPTOKEN_DECIMALS,
+    COMPTOKEN_WALLET_SIZE,
+    GLOBAL_DATA_SIZE,
+    SEC_PER_DAY,
+    DAILY_DISTRIBUTION_HISTORY_SIZE,
+    compto_program_id_pubkey,
+    comptoken_mint_pubkey,
+    global_data_account_pubkey,
+    interest_bank_account_pubkey,
+    verified_human_ubi_bank_account_pubkey,
+    future_ubi_bank_account_pubkey,
+    compto_transfer_hook_id_pubkey,
+    compto_extra_account_metas_account_pubkey,
+} = require("./lib/constants.js");
+
+const {
+    Token,
+    TokenAccount,
+    UserData,
+    UserDataAccount,
+    getComptokenBalance,
+    getNominalOwner,
+    getDistributionOwed,
+    getLastPayoutDate,
+    getDaysSinceLastPayout,
+    isVerifiedHuman,
+    getValidBlockhashes,
+    getHistoricDistributions,
+    GlobalData,
+    GlobalDataAccount,
+} = require("./lib/accounts.js");
+
+const {
+    ComptokenProof,
+    Instruction,
+    createProofSubmissionInstruction,
+    createCreateUserDataAccountInstruction,
+    createDailyDistributionEventInstruction,
+    createGetValidBlockhashesInstruction,
+    createGetOwedComptokensInstruction,
+    createGrowUserDataAccountInstruction,
+    createVerifyHumanInstruction,
+} = require("./lib/instruction.js");
+
+
+
+module.exports = {
+    Token, // accounts.js
+    TokenAccount,
+    UserData,
+    UserDataAccount,
+    getComptokenBalance,
+    getNominalOwner,
+    getDistributionOwed,
+    getLastPayoutDate,
+    getDaysSinceLastPayout,
+    isVerifiedHuman,
+    getValidBlockhashes,
+    getHistoricDistributions,
+    GlobalData,
+    GlobalDataAccount,
+    COMPTOKEN_DECIMALS, // constants.js
+    COMPTOKEN_WALLET_SIZE,
+    GLOBAL_DATA_SIZE,
+    SEC_PER_DAY,
+    DAILY_DISTRIBUTION_HISTORY_SIZE,
+    compto_program_id_pubkey,
+    comptoken_mint_pubkey,
+    global_data_account_pubkey,
+    interest_bank_account_pubkey,
+    verified_human_ubi_bank_account_pubkey,
+    future_ubi_bank_account_pubkey,
+    compto_transfer_hook_id_pubkey,
+    compto_extra_account_metas_account_pubkey,
+    ComptokenProof, // instruction.js
+    Instruction,
+    createProofSubmissionInstruction,
+    createCreateUserDataAccountInstruction,
+    createDailyDistributionEventInstruction,
+    createGetValidBlockhashesInstruction,
+    createGetOwedComptokensInstruction,
+    createGrowUserDataAccountInstruction,
+    createVerifyHumanInstruction,
 };
-exports.__esModule = true;
-__exportStar(require("./lib/accounts.js"), exports);
-__exportStar(require("./lib/constants.js"), exports);
-__exportStar(require("./lib/instruction.js"), exports);
